0e557402a311d50f69db282e109e9e96cc6771da
Daniel Palmer <daniel@0x0f.com>
2022-02-26 00:19:45 +0900

    Bye bye git cdn, you sucked very much.

--- run_woodpecker_agent.sh
+++ run_woodpecker_agent.sh
@@ -2,17 +2,6 @@
 
 source .secrets
 
-docker stop woodpecker_agent_gitcdn
-docker rm woodpecker_agent_gitcdn
-mkdir ~/gitCDN
-docker run -d \
-	-p 7171:8000 \
-	-v $HOME/gitCDN:/git-data \
-	-e GITSERVER_UPSTREAM=https://github.com/ \
-	-e WORKING_DIRECTORY=/git-data \
-	--name woodpecker_agent_gitcdn \
-	registry.gitlab.com/grouperenault/git_cdn
-
 docker stop woodpecker_agent
 docker rm woodpecker_agent
 docker run -d \
